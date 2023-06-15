00091ab49e5de3d9c77822e9c6aaeb2dca0e8d34
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-06-15 11:34:59 +0300

    FirstCommit

--- src/test/java/com/example/demo/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/demo/repository/ProductRepositoryTest.java
@@ -1,10 +1,27 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void saveMethod(){
+        //create product
+        Product product=new Product();
+        product.setName("product 1");
+        product.setDescription("product 1 description");
+        //save product
+        //display product information
+    }
 
 }
