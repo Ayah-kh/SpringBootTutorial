6447be881e144b8682577e7bf03cd3be0eb3402c
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-06-15 11:37:46 +0300

    FirstCommit

--- src/test/java/com/example/demo/repository/ProductRepositoryTest.java
+++ src/test/java/com/example/demo/repository/ProductRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -20,7 +22,14 @@ class ProductRepositoryTest {
         Product product=new Product();
         product.setName("product 1");
         product.setDescription("product 1 description");
+        product.setSku("100ABC");
+        product.setPrice(new BigDecimal(100));
+        product.setActive(true);
+        product.setImageUrl("product1.png");
+
         //save product
+
+
         //display product information
     }
 
